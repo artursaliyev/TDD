4052531d7642b38695dc87c34d4c654e765ff5e3
Artur Saliyev <artur.saliyev@mail.ru>
2019-02-07 19:20:52 +0500

    Add List model

--- lists/models.py
+++ lists/models.py
@@ -18,5 +18,9 @@ class AbstractBoshqarma(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('List', on_delete=models.CASCADE, default=None)
 
+
+class List(models.Model):
+    pass
 
