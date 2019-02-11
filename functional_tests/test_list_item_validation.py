8d6caf29d02e4e0702081a6c378f30fa5fdbe8f8
Artur Saliyev <artur.saliyev@mail.ru>
2019-02-11 19:10:56 +0500

    Разделены модульные тесты на два фйла

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 
 
@@ -7,18 +8,35 @@ class ItemValidationTest(FunctionalTest):
     def test_connot_add_empty_list_items(self):
         """ест: нельзя добавлять пустые элементы списк"""
         # Эдит открывает домашнюю страницу и случайно пытается отправить
-
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
-        # Домашняя страница обновляется, и появляется сообщение об ошибке, # которое говорит,
-        # что элементы списка не должны быть пустыми
-
+        # Домашняя страница обновляется, и появляется сообщение об ошибке,
+        # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You can't have an empty list item"
+        ))
         # Она пробует снова, теперь с неким текстом для элемента, и теперь # это срабатывает
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
-        # Как ни странно, Эдит решает отправить второй пустой элемент списка # Она получает
-        # аналогичное предупреждение на странице списка
+        # Как ни странно, Эдит решает отправить второй пустой элемент списка
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You can't have an empty list item"
+        ))
 
         # И она может его исправить, заполнив поле неким текстом
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
         self.fail('напиши меня!')
 
---
+++ lists/tests/test_models.py
@@ -0,0 +1,68 @@
+from django.http import HttpRequest, HttpResponse
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from lists.models import Item, List
+
+
+class ListAndItemModelTest(TestCase):
+    """тест моделей списка и элемента списк"""
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
+
+
+class NewItemTest(TestCase):
+    """тест нового элемента списка"""
+
+    def test_can_save_a_POST_request_to_an_exiting_list(self):
+        """тест: можно сохранить post-запрос в существующий список"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+                         data={'item_text': 'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """тест: переадресуется в представление списка"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
+                                    data={'item_text': 'A new item for an existing list'})
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
+
