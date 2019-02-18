88accd4ed5c8de9e049ef25dfdaffae26938ce73
Artur Saliyev <artur.saliyev@mail.ru>
2019-02-18 16:31:01 +0500

    Обеспечена валидация модели в представлении списка

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,3 +1,6 @@
+import time
+
+from django.utils.html import escape
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 
@@ -5,20 +8,20 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
     """тест валидации элемента списк"""
 
-    def test_connot_add_empty_list_items(self):
-        """ест: нельзя добавлять пустые элементы списк"""
+    def test_cannot_add_empty_list_items(self):
+        """тест: нельзя добавлять пустые элементы списк"""
         # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
         self.browser.get(self.live_server_url)
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
-        self.wait_for(self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
-        # Она пробует снова, теперь с неким текстом для элемента, и теперь # это срабатывает
+        # Она пробует снова, теперь с неким текстом для элемента, и теперь
+        # это срабатывает
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
@@ -27,8 +30,8 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Она получает аналогичное предупреждение на странице списка
-        self.wait_for(self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
 
@@ -38,5 +41,5 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
-        self.fail('напиши меня!')
+        # self.fail('напиши меня!')
 
